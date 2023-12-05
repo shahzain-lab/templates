a87786fd832cfedea6dad60260bfd9be81e7afd8
shahzain-lab <shahzain30196@gmail.com>
2023-12-05 19:05:59 +0500

    testing lint4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import ButtonWithIcon from "@/shared/ButtonWithIcon";
 import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function Home() {
+  const a = "testt";
   return (
     <main>
-       <Button>Hello o o o o</Button>
-       <ButtonWithIcon Icon={<FaArrowRightLong />}>ButtonWithIcon</ButtonWithIcon>
+      <Button>Hello o o o o O O O O OO </Button>
+      <ButtonWithIcon Icon={<FaArrowRightLong />}>
+        ButtonWithIcon
+      </ButtonWithIcon>
     </main>
-  )
+  );
 }
