f9b8b2b8d20c87c7311017576f22f6d225a37269
shahzain-lab <shahzain30196@gmail.com>
2023-12-05 19:19:19 +0500

    lint fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+import React from "react";
 import { Button } from "@/shared/Button";
 import ButtonWithIcon from "@/shared/ButtonWithIcon";
 import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function Home() {
-  const a = "testt";
   return (
     <main>
-      <Button>Hello o o o o O O O O OO </Button>
+      <Button>Hello o o o o O O O O</Button>
       <ButtonWithIcon Icon={<FaArrowRightLong />}>
         ButtonWithIcon
       </ButtonWithIcon>
