93c388541306b52c07105b5e6a4ce7ed3db35338
shahzain-lab <shahzain30196@gmail.com>
2023-12-09 12:30:24 +0500

    lint fixed

--- src/layout/ToggleDarkMode.tsx
+++ src/layout/ToggleDarkMode.tsx
@@ -1,12 +1,12 @@
-
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
 const DarkModeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
     // Check if dark mode is enabled in local storage
-    const savedDarkMode = localStorage.getItem('darkMode');
+    const savedDarkMode = localStorage.getItem("darkMode");
     if (savedDarkMode) {
       setIsDarkMode(JSON.parse(savedDarkMode));
     }
@@ -14,10 +14,10 @@ const DarkModeToggle = () => {
 
   useEffect(() => {
     // Apply dark mode class to the HTML element
-    document.documentElement.classList.toggle('dark', isDarkMode);
+    document.documentElement.classList.toggle("dark", isDarkMode);
 
     // Save dark mode state to local storage
-    localStorage.setItem('darkMode', JSON.stringify(isDarkMode));
+    localStorage.setItem("darkMode", JSON.stringify(isDarkMode));
   }, [isDarkMode]);
 
   const toggleDarkMode = () => {
